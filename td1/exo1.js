f37820f109ff17b84119395cf6e67bc2e869e95f
YassPirlo <yassine.vitullo@hotmail.com>
2025-12-14 19:05:37 +0100

    14-12

---
+++ td1/exo1.js
@@ -0,0 +1,7 @@
+chienAnimal = 'Rex';
+console.log(chienAnimal); 
+chatAnimal='Pepper';
+console.log(chatAnimal);
+console.log("le nom de mon chien est " + chienAnimal + " et le nom de mon chat est " + chatAnimal);
+sonChat='ronron';
+sonChien='ouaf';
